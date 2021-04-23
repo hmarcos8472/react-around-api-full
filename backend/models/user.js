6f511b2f1601ec2363b1c495263c02a3aae6493f
Marcos <hz.marcosib@gmail.com>
2021-04-23 11:36:41 -0700

    updated userSchema

--- backend/models/user.js
+++ backend/models/user.js
@@ -7,6 +7,13 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
+    validate: {
+      validator: (v) => validator.isEmail(v, {
+        require_tld: true,
+        allow_utf8_local_part: false
+      }),
+      message: 'The email you used is invalid.'
+    }
   },
   password: {
     type: String,
@@ -19,21 +26,21 @@ const userSchema = new mongoose.Schema({
     required: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Jacques Cousteau'
   },
   about: {
     type: String,
     required: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Explorer'
   },
   avatar: {
     type: String,
     required: true,
+    default: 'https://i.imgur.com/jCEMu8X.jpg',
     validate: {
-      validator(v){
-        const urlRegexExpression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi
-        return urlRegexExpression.test(v)
-      },
+      validator: (v) => validator.isURL(v),
       message: "The submitted link is broken"
     }
   }
