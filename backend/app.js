8e782b6bf9727effba9c37a0356a803e2e1b3927
Marcos <hz.marcosib@gmail.com>
2021-04-24 21:51:48 -0700

    rearranged order of loggers

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,15 @@ const mongoose = require('mongoose')
 const app = express();
 const bodyParser = require('body-parser')
 
+const {usersRouter} = require('./routes/users.js')
+const {cardsRouter} = require('./routes/cards.js')
+
 const helmet = require('helmet')
 const cors = require('cors')
 const { celebrate, Joi, errors, Segments } = require('celebrate');
+
+const auth = require('./middleware/auth.js')
+const {requestLogger, errorLogger} = require('./middleware/logger.js')
 
 mongoose.connect('mongodb://localhost:27017/aroundb', {
   useNewUrlParser: true,
@@ -16,11 +22,21 @@ mongoose.connect('mongodb://localhost:27017/aroundb', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.get('/crash-test', () => {
-//   setTimeout(() => {
-//     throw new Error('Server will crash now');
-//   }, 0);
-// });
+// listen to port 3000
+const { PORT = 3000 } = process.env;
+
+const path = require('path')
+
+app.use(cors())
+app.options('*', cors())
+app.use(express.json())
+app.use(helmet())
+app.use(auth)
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public/static')))
+
+app.use(requestLogger)
 
 app.post(
   '/signup',
@@ -44,30 +60,27 @@ app.post(
   login
 );
 
-// listen to port 3000
-const { PORT = 3000 } = process.env;
-
-const path = require('path')
-
-app.use(cors())
-app.options('*', cors())
-app.use(requestLogger)
-app.use(express.json())
-app.use(helmet())
-app.use(auth)
-
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname, 'public/static')))
-
-const {usersRouter} = require('./routes/users.js')
-const {cardsRouter} = require('./routes/cards.js')
-
-const auth = require('./middleware/auth.js')
-const {requestLogger, errorLogger} = require('./middleware/logger.js')
-
 app.use('/', usersRouter)
-
 app.use('/', cardsRouter)
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Server will crash now');
+  }, 0);
+});
+
+app.use(errorLogger)
+app.use(errors())
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res.status(statusCode).send({
+    message: statusCode === 500 ? 'An error occured on the server' : message
+  });
+
+  next();
+});
 
 app.use(function (req, res) {
   res.status(404).send({ message : " Requested Resource Not Found..." });
